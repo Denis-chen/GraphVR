dc67d6237c77e4926aba70cdd450ea183c48f617
GrayTurtle <tran.john1337@gmail.com>
2016-09-25 17:31:02 -0500

    folders of messing around with VR

---
+++ VR_all/app/src/main/java/MainActivity.java
@@ -0,0 +1,19 @@
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by grayturtle on 9/18/16.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        open
+    }
+
+}
+
